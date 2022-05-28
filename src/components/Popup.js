0d7a905cbf0b0acbc4beb26a04cdaef98c8cd7ab
greyguy21 <e0559779@u.nus.edu>
2022-05-28 20:36:40 +0800

    minor edits to css

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,27 +2,30 @@ import React, {useEffect} from 'react'
 import { checkWin } from '../helpers/helpers';
 
 const Popup = ({correctLetters, wrongLetters, selectedWord, setPlayable, playAgain}) => {
-    let finalMessage = '';
+    let finalMessage1 = '';
+    let finalMessage2 = '';
     let finalMessageRevealWord = '';
     let playable = true;
     let status = checkWin(correctLetters, wrongLetters, selectedWord);
 
     if (status == 'win') {
-        finalMessage = 'Congratulations! you won!';
+        finalMessage1 = 'Congratulations! you won!';
         playable = false;
     } else if (status == 'lose') {
-        finalMessage = 'Unfortunately you lost.';
-        finalMessageRevealWord = `...the word was ${selectedWord}`;
+        finalMessage1 = 'Unfortunately you lost :(';
+        finalMessage2 = 'The word was ...'
+        finalMessageRevealWord = `${selectedWord}`;
         playable = false;
     } 
 
     useEffect(() => setPlayable(playable));
 
     return (
-    <div className="popup-container" style={finalMessage != '' ? {display:'flex'}: {}}>
+    <div className="popup-container" style={finalMessage1 != '' ? {display:'flex'}: {}}>
         <div className="popup">
-            <h2>{finalMessage}</h2>
-            <h3>{finalMessageRevealWord}</h3>
+            <h2>{finalMessage1}</h2>
+            <h3>{finalMessage2}</h3>
+            <h2 id='wordReveal'>{finalMessageRevealWord}</h2>
             <button onClick={playAgain}>Play Again</button>
         </div>
     </div>
